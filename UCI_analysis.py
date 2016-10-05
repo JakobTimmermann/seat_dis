aac315f30a817803e78a6be45e041fbb8f556b4f
Daisy <jason-ti@web.de>
2016-10-05 20:06:48 +0200

    Add Audition seperation

--- UCI_analysis.py
+++ UCI_analysis.py
@@ -12,18 +12,46 @@ data.show_start = data.show_start.dt.date
 regular = data[data.show_start<pd.datetime(2015,8,12).date()]
 dynamic = data[data.show_start>=pd.datetime(2016,3,31).date()]
 
-avg_row = pd.DataFrame(regular.groupby(['show_start']).mean()['seat_row'])
-avg_row_dyn = pd.DataFrame(dynamic.groupby(['show_start']).mean()['seat_row'][:-1])
-avg_row.rename(columns= {'seat_row':'reg'},inplace=True)
-avg_row_dyn.rename(columns= {'seat_row':'dyn'},inplace=True)
-avg_row['dyn']=avg_row_dyn.dyn.values
-avg_row['difference'] = avg_row.dyn.values-avg_row.reg.values
+avg_row = pd.DataFrame(regular.groupby(['show_start']).sum()['seat_row'])
+avg_row_dyn = pd.DataFrame(dynamic.groupby(['show_start']).sum()['seat_row'][:-1])
+
+avg_row.rename(columns= {'seat_row':'avg_row_per_day'},inplace=True)
+avg_row_dyn.rename(columns= {'seat_row':'avg_row_per_day'},inplace=True)
+avg_row['tickets_per_day'] = pd.DataFrame(regular.groupby(['show_start']).size())
+avg_row_dyn['tickets_per_day'] = pd.DataFrame(dynamic.groupby(['show_start']).size())
+
+#avg_row['dyn']=avg_row_dyn.dyn.values
+#avg_row['difference'] = avg_row.dyn.values-avg_row.reg.values
 
 weekly = avg_row.reset_index()
-weekly = weekly.groupby(pd.to_datetime(weekly.show_start).dt.week).mean()
+weekly = weekly.groupby(pd.to_datetime(weekly.show_start).dt.week).sum()
+weekly_dyn = avg_row_dyn.reset_index()
+weekly_dyn = weekly_dyn.groupby(pd.to_datetime(weekly_dyn.show_start).dt.week).sum()
+weekly = weekly.reset_index()
+weekly_dyn = weekly_dyn.reset_index()
+weekly['av_reg'] = weekly.avg_row_per_day/ weekly.tickets_per_day
+weekly['av_dyn'] = weekly_dyn.avg_row_per_day/ weekly_dyn.tickets_per_day
 weekly.plot()
 
-temp = data.groupby(['auditorium_no','ext_show_id']).size()
+## Different Auditoriums
+avg_row_aud = pd.DataFrame(regular.groupby(['show_start','auditorium_no']).sum()['seat_row'])
+avg_row_aud_dyn = pd.DataFrame(dynamic.groupby(['show_start','auditorium_no']).sum()['seat_row'][:-1])
+avg_row_aud.rename(columns= {'seat_row':'avg_row_per_day'},inplace=True)
+avg_row_aud_dyn.rename(columns= {'seat_row':'avg_row_per_day'},inplace=True)
+avg_row_aud['tickets_per_day'] = pd.DataFrame(regular.groupby(['show_start','auditorium_no']).size())
+avg_row_aud_dyn['tickets_per_day'] = pd.DataFrame(dynamic.groupby(['show_start','auditorium_no']).size())
+
+weekly_aud = avg_row_aud.reset_index()
+weekly_aud = weekly_aud.groupby([pd.to_datetime(weekly_aud.show_start).dt.week,'auditorium_no']).sum()
+weekly_dyn_aud = avg_row_aud_dyn.reset_index()
+weekly_dyn_aud = weekly_dyn_aud.groupby([pd.to_datetime(weekly_dyn_aud.show_start).dt.week,'auditorium_no']).sum()
+weekly_aud = weekly_aud.reset_index()
+weekly_dyn_aud = weekly_dyn_aud.reset_index()
+weekly_aud['av_reg'] = weekly_aud.avg_row_per_day/ weekly_aud.tickets_per_day
+weekly_aud['av_dyn'] = weekly_dyn_aud.avg_row_per_day/ weekly_dyn_aud.tickets_per_day
+
+
+
 #cap = []
 #for k in temp.index.levels[0]:
 #    cap.append(temp[k].max())
