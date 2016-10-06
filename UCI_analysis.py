5a47bf31b4f1058d4c330b13851dece14db99fda
Daisy <jason-ti@web.de>
2016-10-06 12:42:12 +0200

    Add Plot + Start on Capacities

--- UCI_analysis.py
+++ UCI_analysis.py
@@ -1,11 +1,13 @@
 import pandas as pd 
 import numpy as np
 import pdb
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pl
 import scipy.stats as sst
 
 
-def HypTest(data):
+def HypTest(data,aud_no):
+	temp = data_m[['av_reg_day_aud','av_reg_day_aud_dyn']][data_m.auditorium_no_x == aud_no]
+	temp.rename(columns= {'av_reg_day_aud':'av_reg','av_reg_day_aud_dyn':'av_dyn'},inplace=True)
 	# Testing for normal distribution
 	print('    output normaltest for regular') 
 	print(sst.normaltest(data.av_reg))
@@ -15,9 +17,13 @@ def HypTest(data):
 	# Zweistichproben Gauss Test : https://de.wikipedia.org/wiki/Gau%C3%9F-Test
 	# 
 	reg_td_av, dyn_td_av = data.mean()[:2]
+	print(reg_td_av)
+	print(dyn_td_av)
 	n = len(data.av_reg)
 	reg_dev = np.sqrt(((data.av_reg - reg_td_av)**2).values.sum()/(len(data.av_reg)-1))
 	dyn_dev = np.sqrt(((data.av_dyn - dyn_td_av)**2).values.sum()/(len(data.av_dyn)-1))
+	print(reg_dev)
+	print(dyn_dev)
 
 	Z=(dyn_td_av-reg_td_av)/np.sqrt((dyn_dev)**2/n + (reg_dev)**2/n)
 
@@ -28,6 +34,7 @@ def HypTest(data):
 	# Welch-Test https://en.wikipedia.org/wiki/Welch%27s_t-test
 	#
 	#sst.ttest_ind_from_stats(reg_td_av,reg_dev,134,dyn_td_av,dyn_dev,134)
+	#sst.kstest()
 
 
 #data = pd.read_csv('2015_2016_clean_uci_potsdam.csv', usecols=['show_start','auditorium_no','seat_number', 'seat_row'],parse_dates=['show_start'],nrows=1000)
@@ -71,17 +78,52 @@ weekly_aud['av_dyn'] = weekly_aud.tot_row_val_dynamic/ weekly_aud.tickets_per_da
 
 weekly_total['av_reg'] = weekly_total.tot_row_val/ weekly_total.tickets_per_day_per_aud
 weekly_total['av_dyn'] = weekly_total.tot_row_val_dynamic/ weekly_total.tickets_per_day_per_aud_dynamic
+weekly_total.drop(['auditorium_no'],axis=1,inplace=True)
+weekly_total.rename(columns= {'tickets_per_day_per_aud':'tickets_per_week','tickets_per_day_per_aud_dynamic':'tickets_per_week_dynamic'},inplace=True)
 
-aud_no = 3
-temp =  weekly_aud[['av_reg','av_dyn']][weekly_aud.auditorium_no == aud_no]
-temp['diff'] = temp.av_dyn - temp.av_reg
-temp.plot()
+def plot_weekly_total(weekly_total):
+	x = weekly_total.index
+	av_dyn, av_reg = weekly_total[['av_dyn','av_reg']].values.transpose()
+	att_dyn, att_reg = weekly_total[['tickets_per_week_dynamic','tickets_per_week']].values.transpose()
+	att_dyn, att_reg = att_dyn/max(att_reg)*4, att_reg/max(att_reg)*4
+	pl.plot(x,av_dyn,label='average row dynamic pricing')
+	pl.plot(x,av_reg,label='average row regular pricing')
 
-data = pd.DataFrame(avg_row_aud.tot_row_val/ avg_row_aud.tickets_per_day_per_aud)
-data.rename(columns= {0:'av_reg_day_aud'},inplace=True)
-data = data.reset_index()
-data.show_start = pd.to_datetime(data.show_start)
-data['ind'] = 1000*data.show_start.dt.month+10*data.show_start.dt.day+data.auditorium_no
+	pl.plot(x,att_dyn,label='attendance dynamic')
+	pl.plot(x,att_reg,label='attendance regular')
+	pl.legend(loc='upper left')
+	pl.ylim([0,12])
+	pl.xlabel('Calender Week')
+	pl.ylabel('row number')
+	pl.show()
+	
+
+def plot_weekly_aud(weekly_aud, aud_no):
+	aud_cap = [None,187,299,431,167,184,326,298,187]
+	temp =  weekly_aud[['av_reg','av_dyn','tickets_per_day_per_aud','tickets_per_day_per_aud_dynamic']][weekly_aud.auditorium_no == aud_no]
+	temp['diff'] = temp.av_dyn - temp.av_reg
+	x = weekly_total.index
+	av_dyn, av_reg = temp[['av_dyn','av_reg']].values.transpose()
+	att_dyn, att_reg = temp[['tickets_per_day_per_aud_dynamic','tickets_per_day_per_aud']].values.transpose()
+	att_dyn, att_reg = att_dyn/max(att_reg)*4, att_reg/max(att_reg)*4
+	pl.plot(x,av_dyn,label='average row dynamic pricing')
+	pl.plot(x,av_reg,label='average row regular pricing')
+
+	pl.plot(x,att_dyn,label='attendance dynamic')
+	pl.plot(x,att_reg,label='attendance regular')
+	pl.legend(loc='upper left')
+	pl.ylim([0,12])
+	pl.xlabel('Calender Week')
+	pl.ylabel('row number')
+	pl.title('Auditorium %i'%aud_no, fontsize=18, fontweight='bold')
+	pl.text(25,11,'Capacity: %i' %(aud_cap[aud_no]),fontsize=15)
+	pl.show()
+
+data_r = pd.DataFrame(avg_row_aud.tot_row_val/ avg_row_aud.tickets_per_day_per_aud)
+data_r.rename(columns= {0:'av_reg_day_aud'},inplace=True)
+data_r = data_r.reset_index()
+data_r.show_start = pd.to_datetime(data_r.show_start)
+data_r['ind'] = 1000*data_r.show_start.dt.month+10*data_r.show_start.dt.day+data_r.auditorium_no
 
 data_d = pd.DataFrame(avg_row_aud_dyn.tot_row_val_dynamic/ avg_row_aud_dyn.tickets_per_day_per_aud_dynamic)
 data_d.rename(columns= {0:'av_reg_day_aud_dyn'},inplace=True)
@@ -89,32 +131,16 @@ data_d = data_d.reset_index()
 data_d.show_start = pd.to_datetime(data_d.show_start)
 data_d['ind'] = 1000*data_d.show_start.dt.month+10*data_d.show_start.dt.day+data_d.auditorium_no
 
-data = data.merge(data_d, on='ind')  
-data.drop(['auditorium_no_y','show_start_x','ind'],axis=1,inplace=True)
+data_m = data_r.merge(data_d, on='ind')  
+data_m.drop(['auditorium_no_y','show_start_x','ind'],axis=1,inplace=True)
 
-temp = data[['av_reg_day_aud','av_reg_day_aud_dyn']][data.auditorium_no_x == aud_no]
-temp.rename(columns= {'av_reg_day_aud':'av_reg','av_reg_day_aud_dyn':'av_dyn'},inplace=True)
+#HypTest(data_m,1)
 
-HypTest(temp)
+aud_cap = pd.DataFrame(data={'capacities':[187,299,431,167,184,326,298,187],'auditorium_no':[1,2,3,4,5,6,7,8]})
+shows_aus = pd.DataFrame(data.groupby(['ext_show_id','auditorium_no']).size())
+shows_aus.rename(columns= {0:'visitors'},inplace=True)
+shows_aus = shows_aus.reset_index()
+shows_aus = shows_aus.merge(aud_cap)
+shows_aus['Auslastung'] = shows_aus.visitors/shows_aus.capacities
 
-plt.show()
 
-
-'''
->>>>>>> master
-aud_cap = pd.DataFrame(data={'capacities':[187,299,431,167,184,326,298,187]},index=temp.index.levels[0])
-temp = temp.reset_index().merge(aud_cap.reset_index())
-temp.rename(columns = {0:'Visitors'},inplace=True)
-temp['Auslastung'] = temp.Visitors/temp.capacities
-
-dynamic = dynamic.merge(temp[['Auslastung','ext_show_id']])
-regular = regular.merge(temp[['Auslastung','ext_show_id']])
-
-avg_row_tot = pd.DataFrame(data.groupby(['show_start']).mean()['seat_row'][:-1])
-grain = avg_row_tot.reset_index()
-grain.show_start = pd.to_datetime(grain.show_start).dt.date
-grain = grain[(grain.show_start<pd.datetime(2016,3,1).date()) & (grain.show_start>pd.datetime(2015,5,1).date())]
-grain = grain.groupby(pd.to_datetime(grain.show_start).dt.week).mean()
-grain.plot()
-average_row_aud = data.groupby(['show_start','auditorium_no']).mean()['seat_row'][:-1]
-'''
